f6936fa3c050c7a58cfb5dfa1f56528997e9a71e
Sepehr <ahmadipoursepehr@gmail.com>
2022-11-07 14:15:30 -0800

    Added the timer file and changed the button colors a little

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './Styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import HomeScreen from "./Pages/Home";
-import {Timer} from "./Pages/Timer";
+import Timer from "./Pages/Timer";
 
 function App() {
     return (
---
+++ src/Pages/Timer.jsx
@@ -0,0 +1,8 @@
+export default function Timer(){
+    return(
+        <h1>
+            Hello there my friend
+        </h1>
+    )
+
+}
