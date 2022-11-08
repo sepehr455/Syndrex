bb60841f5dad7c60f7617d34155e9c12b7617a07
Sepehr <ahmadipoursepehr@gmail.com>
2022-11-07 18:09:44 -0800

    Added some style and icons for the timer functionality

--- src/Pages/Timer.jsx
+++ src/Pages/Timer.jsx
@@ -1,8 +1,15 @@
-export default function Timer(){
-    return(
-        <h1>
-            Hello there my friend
-        </h1>
+export default function Timer() {
+    return (
+        <div className="timer">
+            <span className="timer__part timer__part--minutes">00</span>
+            <span className="timer__part">:</span>
+            <span className="timer__part timer__part--seconds">00</span>
+            <button type={"button"} className="timer__btn timer__btn--control timer__button--start">
+                <span className="material-icons">play_arrow</span>
+            </button>
+            <button type={"button"} className="timer__btn timer__btn--reset">
+                <span className="material-icons">timer</span>
+            </button>
+        </div>
     )
-
 }
