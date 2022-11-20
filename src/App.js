7c80638b3d0e46ff4e83468a0aaf8527bfc4e499
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-11-20 15:52:10 -0800

    added a basic timer but still needs lots of work

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import HomeScreen from "./Pages/Home";
 import Timer from "./Pages/Timer";
 
+
 function App() {
+
     return (
         <Router>
             <Routes>
                 <Route path = "/" element={<HomeScreen/>} />
-                <Route path= "/timer" element={<Timer />}/>
+                <Route path= "/timer" element={<Timer/>}/>
             </Routes>
         </Router>
 
