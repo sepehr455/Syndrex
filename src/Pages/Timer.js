42c9c780f61c0f25602341e8333b34c478aa8980
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-11-21 13:49:20 -0800

    timer with start and stop functionality without any css

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -1,106 +1,81 @@
-import {useEffect, useRef, useState} from "react";
-import React from "react";
-import {queries} from "@testing-library/react";
-
-const Timer = () => {
-
-    //a hook that will be used to pause the timer
-    const [status, setStatus] = React.useState("working");
-
-    // We need ref in this, because we are dealing
-    // with JS setInterval to keep track of it and
-    // stop it when needed
-    const Ref = useRef(null);
-
-    // The state for our timer
-    const [timer, setTimer] = useState('00:00:00');
+import React, {useState, useEffect, useRef} from 'react'
+import GradientButton from "../Components/GradientButton";
+import {Button} from "react-bootstrap";
 
 
-    const getTimeRemaining = (e) => {
-        const total = Date.parse(e) - Date.parse(new Date());
-        const seconds = Math.floor((total / 1000) % 60);
-        const minutes = Math.floor((total / 1000 / 60) % 60);
-        const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-        return {
-            total, hours, minutes, seconds
-        };
+const STATUS = {
+    STARTED: 'Started',
+    STOPPED: 'Stopped',
+}
+
+const INITIAL_COUNT = 120
+
+export default function Timer() {
+    const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT)
+    const [status, setStatus] = useState(STATUS.STOPPED)
+
+    const secondsToDisplay = secondsRemaining % 60
+    const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60
+    const minutesToDisplay = minutesRemaining % 60
+    const hoursToDisplay = (minutesRemaining - minutesToDisplay) / 60
+
+    const handleStart = () => {
+        setStatus(STATUS.STARTED)
     }
-
-
-    const startTimer = (e) => {
-        let {total, hours, minutes, seconds}
-            = getTimeRemaining(e);
-        if (total >= 0) {
-
-            // check if less than 10 then we need to
-            // add '0' at the beginning of the variable
-            setTimer(
-                (hours > 9 ? hours : '0' + hours) + ':' +
-                (minutes > 9 ? minutes : '0' + minutes) + ':'
-                + (seconds > 9 ? seconds : '0' + seconds)
-            )
-        }
-
+    const handleStop = () => {
+        setStatus(STATUS.STOPPED)
     }
-
-    const clearTimer = (e) => {
-
-        //also edit line 61 if you change this
-        setTimer('00:01:10');
-
-        if (Ref.current) clearInterval(Ref.current);
-
-        Ref.current = setInterval(() => {
-            startTimer(e);
-        }, 1000);
+    const handleReset = () => {
+        setStatus(STATUS.STOPPED)
+        setSecondsRemaining(INITIAL_COUNT)
     }
-
-    const getDeadTime = () => {
-        let deadline = new Date();
-        deadline.setSeconds(deadline.getSeconds() + 70);
-        return deadline;
-    }
-
-    // We can use useEffect so that when the component
-    // mount the timer will start as soon as possible
-
-    // We put empty array to act as componentDid
-    // mount only
-    useEffect(() => {
-        if(status === 'working'){
-            clearTimer(getDeadTime());
-        } else {
-            clearTimeout(Ref.current);
-        }
-
-    }, [status]);
-
-    const stopTimer = () => {
-        setStatus("paused");
-    }
-
-    const resume = () => {
-        setStatus("working");
-    }
-
-
-    // Another way to call the clearTimer() to start
-    // the countdown is via action event from the
-    // button first we create function to be called
-    // by the button
-    const onClickReset = () => {
-        clearTimer(getDeadTime());
-    }
-
+    useInterval(
+        () => {
+            if (secondsRemaining > 0) {
+                setSecondsRemaining(secondsRemaining - 1)
+            } else {
+                setStatus(STATUS.STOPPED)
+            }
+        },
+        status === STATUS.STARTED ? 1000 : null,
+        // passing null stops the interval
+    )
     return (
         <div className="App">
-            <h2 id="counter">{timer}</h2>
-            <button onClick={onClickReset}>Reset</button>
-            <button>Start</button>
-            <button onClick={stopTimer}>Stop</button>
 
+            <div style={{padding: 20}}>
+                {twoDigits(hoursToDisplay)}:{twoDigits(minutesToDisplay)}:
+                {twoDigits(secondsToDisplay)}
+            </div>
+            <button onClick={handleStart} type="button" className="timer__buttons">
+                Start
+            </button>
+            <button onClick={handleStop} type="button" className="timer__buttons">
+                Stop
+            </button>
+            <button onClick={handleReset} type="button" className="timer__buttons">
+                Reset
+            </button>
         </div>
     )
 }
 
-export default Timer;
+function useInterval(callback, delay) {
+    const savedCallback = useRef()
+
+    useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    useEffect(() => {
+        function tick() {
+            savedCallback.current()
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay)
+            return () => clearInterval(id)
+        }
+    }, [delay])
+}
+
+const twoDigits = (num) => String(num).padStart(2, '0')
