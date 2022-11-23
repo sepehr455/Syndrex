b440039d90ec6171490364669912ee726b01e35c
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-11-23 14:01:56 -0800

    added the change time button without the functionality

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -18,9 +18,17 @@ export default function Timer() {
 
     const handleStart = () => {
         setStatus(STATUS.STARTED)
+        document.querySelector('.start-button').setAttribute("disabled", "true")
+        document.querySelector('.stop-button').removeAttribute("disabled")
     }
     const handleStop = () => {
         setStatus(STATUS.STOPPED)
+        if (!!document.querySelector('#counter')){
+            document.querySelector('#counter').remove()
+        }
+        document.querySelector('.stop-button').setAttribute("disabled", "true")
+        document.querySelector('.start-button').removeAttribute("disabled")
+        document.querySelector('.pause-button').remove()
     }
     const handleReset = () => {
         setStatus(STATUS.STOPPED)
@@ -40,14 +48,14 @@ export default function Timer() {
     return (
         <div className="timer_div">
 
-            <div className= "timer_nums">
+            <div className= "timer_nums counter">
                 {twoDigits(hoursToDisplay)}:{twoDigits(minutesToDisplay)}:
                 {twoDigits(secondsToDisplay)}
             </div>
-            <button onClick={handleStart} type="button" className="timer__buttons">
+            <button onClick={handleStart} type="button" className="timer__buttons start-button">
                 Start
             </button>
-            <button onClick={handleStop} type="button" className="timer__buttons">
+            <button onClick={handleStop} type="button" className="timer__buttons stop-button">
                 Stop
             </button>
             <button onClick={handleReset} type="button" className="timer__buttons">
